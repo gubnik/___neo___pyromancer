e5dab149f2115826237378a77b0874b6caac7ee8
nikgub_ <danger8217.nik@gmail.com>
2024-02-02 00:56:45 +0300

    Removed legacy code

--- src/main/java/xyz/nikgub/pyromancer/entities/attack_effects/flaming_guillotine/FlamingGuillotineAnimations.java
+++ src/main/java/xyz/nikgub/pyromancer/entities/attack_effects/flaming_guillotine/FlamingGuillotineAnimations.java
@@ -1,25 +1,8 @@
 package xyz.nikgub.pyromancer.entities.attack_effects.flaming_guillotine;
 
 import net.minecraft.client.animation.AnimationDefinition;
-import xyz.nikgub.pyromancer.animations.from_text.PyranimParser;
+import xyz.nikgub.pyromancer.animations.from_text.Pyranim;
 
 public class FlamingGuillotineAnimations {
-    public static final AnimationDefinition FALL = new PyranimParser("guillotine.pyranim").create();
-            //= AnimationDefinition.Builder.withLength(0.5f)
-            //.addAnimation("bone",
-            //        new AnimationChannel(AnimationChannel.Targets.POSITION,
-            //                new Keyframe(0f, KeyframeAnimations.posVec(0f, 36f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM),
-            //                new Keyframe(0.35f, KeyframeAnimations.posVec(0f, 42f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM),
-            //                new Keyframe(0.5f, KeyframeAnimations.posVec(0f, -16f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM)))
-            //.addAnimation("bone",
-            //        new AnimationChannel(AnimationChannel.Targets.ROTATION,
-            //                new Keyframe(0f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM),
-            //                new Keyframe(0.35f, KeyframeAnimations.degreeVec(0f, -15f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM),
-            //                new Keyframe(0.5f, KeyframeAnimations.degreeVec(0f, 360f, 0f),
-            //                        AnimationChannel.Interpolations.CATMULLROM))).build();
+    public static final AnimationDefinition FALL = Pyranim.ofEntity("guillotine.pyranim");
 }
