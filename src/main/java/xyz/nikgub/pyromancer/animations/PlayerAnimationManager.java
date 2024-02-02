bf375af69a68089e467ed0930b6c9128f22a4147
nikgub_ <danger8217.nik@gmail.com>
2024-02-02 16:08:54 +0300

    Changed stack to map for more consistent behaviour

--- src/main/java/xyz/nikgub/pyromancer/animations/PlayerAnimationManager.java
+++ src/main/java/xyz/nikgub/pyromancer/animations/PlayerAnimationManager.java
@@ -3,39 +3,33 @@ package xyz.nikgub.pyromancer.animations;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.world.entity.player.Player;
 
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <h1>How does it work?</h1>
- * <p>When you launch an animation via a method, AnimationInstance containing info about launched animation will be pushed onto a stack.</p>
- * <p>Then player's model will try to consume the animation on top of the stack for its player, popping it if successful.
- * Thus, stack acts more like a fool-proof impulse signal, where animations for different players will have next to no delay in playing</p>
- * <p>Due to the tick-based nature of animation handling there is no situation when this fails, unless specifically meant to be failed (launched for fake player, launched with null animation etc.)</p>
+ * <h2>How does it work?</h2>
+ * <p>When you launch an animation via a method, AnimationInstance containing info about launched animation will be put into a hash map.</p>
+ * <p>Then player's model will try to consume the animation for its player, making it null if successful.</p>
  * @author nikgub_
  */
 @SuppressWarnings("unused")
 public class PlayerAnimationManager {
 
-    private static final Stack<AnimationInstance> runningAnimations = new Stack<>();
-
-    public static boolean isIdle()
-    {
-        return runningAnimations.empty();
-    }
+    private static final Map<Player, AnimationInstance> runningAnimations = new HashMap<>();
 
     public static void launchAnimation(Player player, AnimationDefinition animationDefinition, boolean override)
     {
-        PlayerAnimationManager.runningAnimations.push(new AnimationInstance(player, animationDefinition, override));
+        PlayerAnimationManager.runningAnimations.put(player, new AnimationInstance(animationDefinition, override));
     }
 
     public static AnimationInstance consumeAnimationFor(Player player)
     {
-        if(isIdle()) return null;
-        if(!(runningAnimations.peek().player.equals(player))) return null;
-        else return runningAnimations.pop();
+        AnimationInstance instance = runningAnimations.get(player);
+        runningAnimations.put(player, null);
+        return instance;
     }
 
-    public record AnimationInstance(Player player, AnimationDefinition animation, boolean override)
+    public record AnimationInstance(AnimationDefinition animation, boolean override)
     {
     }
 }
