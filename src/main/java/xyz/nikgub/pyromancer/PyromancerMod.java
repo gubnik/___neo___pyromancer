c47ffc19d117144749ede97f862d261c5ed72e4b
nikgub_ <danger8217.nik@gmail.com>
2024-03-17 15:16:55 +0300

    License change

--- src/main/java/xyz/nikgub/pyromancer/PyromancerMod.java
+++ src/main/java/xyz/nikgub/pyromancer/PyromancerMod.java
@@ -1,3 +1,21 @@
+/*
+    Pyromancer, Minecraft Forge modification
+    Copyright (C) 2024, nikgub_
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package xyz.nikgub.pyromancer;
 
 import com.mojang.logging.LogUtils;
@@ -250,7 +268,7 @@ public class PyromancerMod
         @SubscribeEvent
         public static void livingHurtEvent(LivingHurtEvent event)
         {
-            if (!(event.getSource().getEntity() instanceof LivingEntity sourceEntity)) return;
+            if (!(event.getSource().getEntity() instanceof Player sourceEntity)) return;
             LivingEntity entity = event.getEntity();
             double armor_pierce = sourceEntity.getAttributeValue(AttributeRegistry.ARMOR_PIERCING.get());
             if (armor_pierce >= entity.getAttributeValue(Attributes.ARMOR))
