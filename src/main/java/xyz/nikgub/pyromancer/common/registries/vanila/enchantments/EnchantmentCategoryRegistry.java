e8f17e0e88d7d86d79e5b122648245b8eb1b98af
nikgub_ <danger8217.nik@gmail.com>
2024-06-02 21:44:37 +0300

    fixed some weird stuff

--- src/main/java/xyz/nikgub/pyromancer/common/registries/vanila/enchantments/EnchantmentCategoryRegistry.java
+++ src/main/java/xyz/nikgub/pyromancer/common/registries/vanila/enchantments/EnchantmentCategoryRegistry.java
@@ -1,9 +1,10 @@
 package xyz.nikgub.pyromancer.common.registries.vanila.enchantments;
 
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import xyz.nikgub.pyromancer.common.items.MaceItem;
 import xyz.nikgub.pyromancer.common.items.BlazingJournalItem;
+import xyz.nikgub.pyromancer.common.items.MaceItem;
 
 public class EnchantmentCategoryRegistry {
     public static final EnchantmentCategory BLAZING_JOURNAL = EnchantmentCategory.create("blazing_journal", (item -> item instanceof BlazingJournalItem));
+    public static final EnchantmentCategory MACE = EnchantmentCategory.create("mace", (item -> item instanceof MaceItem));
 }
