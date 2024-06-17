74775acf31ae7df2496c6755c095b2120694cd80
nikgub_ <danger8217.nik@gmail.com>
2024-06-17 19:28:10 +0300

    Deleted a redundant shadow field

--- src/main/java/xyz/nikgub/pyromancer/mixin/client/ItemInHandRendererMixin.java
+++ src/main/java/xyz/nikgub/pyromancer/mixin/client/ItemInHandRendererMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
@@ -22,9 +21,7 @@ import xyz.nikgub.pyromancer.common.ember.Ember;
 @SuppressWarnings("unused")
 @Mixin(ItemInHandRenderer.class)
 public abstract class ItemInHandRendererMixin {
-    @Final
-    @Shadow
-    private ItemRenderer itemRenderer;
+
     @Shadow
     public abstract void renderItem(LivingEntity entity, ItemStack itemStack, ItemDisplayContext displayContext, boolean b, PoseStack poseStack, MultiBufferSource bufferSource, int i);
     @Shadow
