506fcc54daf03b27d8070704a19eb16898f276f7
nikgub_ <danger8217.nik@gmail.com>
2024-06-17 19:28:47 +0300

    Renamed Flame Respite effect to Fiery Aegis

--- src/main/java/xyz/nikgub/pyromancer/common/registries/MobEffectRegistry.java
+++ src/main/java/xyz/nikgub/pyromancer/common/registries/MobEffectRegistry.java
@@ -5,11 +5,11 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import xyz.nikgub.pyromancer.PyromancerMod;
-import xyz.nikgub.pyromancer.common.mob_effects.FlameRespiteEffect;
+import xyz.nikgub.pyromancer.common.mob_effects.FieryAegisEffect;
 import xyz.nikgub.pyromancer.common.mob_effects.MeltdownEffect;
 
 public class MobEffectRegistry {
     public static DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, PyromancerMod.MOD_ID);
     public static RegistryObject<MeltdownEffect> MELTDOWN = MOB_EFFECTS.register("meltdown", MeltdownEffect::new);
-    public static RegistryObject<FlameRespiteEffect> FLAME_RESPITE = MOB_EFFECTS.register("flame_respite", FlameRespiteEffect::new);
+    public static RegistryObject<FieryAegisEffect> FIERY_AEGIS = MOB_EFFECTS.register("fiery_aegis", FieryAegisEffect::new);
 }
