82426089cfedabdc49f4730fa19fbca5892aee84
nikgub_ <danger8217.nik@gmail.com>
2024-06-19 00:10:44 +0300

    Added Solar Collision effect

--- src/main/java/xyz/nikgub/pyromancer/common/registries/MobEffectRegistry.java
+++ src/main/java/xyz/nikgub/pyromancer/common/registries/MobEffectRegistry.java
@@ -7,9 +7,11 @@ import net.minecraftforge.registries.RegistryObject;
 import xyz.nikgub.pyromancer.PyromancerMod;
 import xyz.nikgub.pyromancer.common.mob_effects.FieryAegisEffect;
 import xyz.nikgub.pyromancer.common.mob_effects.MeltdownEffect;
+import xyz.nikgub.pyromancer.common.mob_effects.SolarCollisionEffect;
 
 public class MobEffectRegistry {
     public static DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, PyromancerMod.MOD_ID);
     public static RegistryObject<MeltdownEffect> MELTDOWN = MOB_EFFECTS.register("meltdown", MeltdownEffect::new);
     public static RegistryObject<FieryAegisEffect> FIERY_AEGIS = MOB_EFFECTS.register("fiery_aegis", FieryAegisEffect::new);
+    public static RegistryObject<SolarCollisionEffect> SOLAR_COLLISION = MOB_EFFECTS.register("solar_collision", SolarCollisionEffect::new);
 }
