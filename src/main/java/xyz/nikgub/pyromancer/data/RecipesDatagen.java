46bcf24fa791983984c4fe37acbfc6adc7611e17
nikgub_ <danger8217.nik@gmail.com>
2024-06-26 04:17:03 +0300

    Added marauder armor recipes

--- src/main/java/xyz/nikgub/pyromancer/data/RecipesDatagen.java
+++ src/main/java/xyz/nikgub/pyromancer/data/RecipesDatagen.java
@@ -1,6 +1,7 @@
 package xyz.nikgub.pyromancer.data;
 
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.tags.ItemTags;
@@ -22,62 +23,94 @@ public class RecipesDatagen extends RecipeProvider {
                 .requires(Items.BLAZE_POWDER)
                 .requires(Items.BOOK)
                 .unlockedBy("blaze_powder", InventoryChangeTrigger.TriggerInstance.hasItems(Items.BLAZE_POWDER))
-                .save(consumer, "blazing_journal");
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.WOODEN_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', ItemTags.PLANKS)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "wooden_mace");
+                .unlockedBy("planks", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(ItemTags.PLANKS).build()))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.STONE_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', ItemTags.STONE_TOOL_MATERIALS)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "stone_mace");
+                .unlockedBy("stone", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(ItemTags.STONE_TOOL_MATERIALS).build()))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.IRON_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', Items.IRON_INGOT)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "iron_mace");
+                .unlockedBy("iron_ingot", InventoryChangeTrigger.TriggerInstance.hasItems(Items.IRON_INGOT))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.GOLDEN_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', Items.GOLD_INGOT)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "golden_mace");
+                .unlockedBy("gold_ingot", InventoryChangeTrigger.TriggerInstance.hasItems(Items.GOLD_INGOT))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.DIAMOND_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', Items.DIAMOND)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "diamond_mace");
+                .unlockedBy("diamond", InventoryChangeTrigger.TriggerInstance.hasItems(Items.DIAMOND))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.NETHERITE_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', Items.NETHERITE_INGOT)
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "netherite_mace");
+                .unlockedBy("netherite", InventoryChangeTrigger.TriggerInstance.hasItems(Items.NETHERITE_INGOT))
+                .save(consumer);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.AMBER_MACE.get())
                 .pattern("aaa")
                 .pattern("asa")
                 .pattern(" s ")
                 .define('a', ItemRegistry.AMBER.get())
                 .define('s', Items.STICK)
-                .unlockedBy("stick", InventoryChangeTrigger.TriggerInstance.hasItems(Items.STICK))
-                .save(consumer, "amber_mace");
+                .unlockedBy("amber", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.AMBER.get()))
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ItemRegistry.AMBER_AXE.get()).define('#', Items.STICK).define('X', ItemRegistry.AMBER.get()).pattern("XX").pattern("X#").pattern(" #").unlockedBy("amber", has(ItemRegistry.AMBER.get())).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ItemRegistry.AMBER_HOE.get()).define('#', Items.STICK).define('X', ItemRegistry.AMBER.get()).pattern("XX").pattern(" #").pattern(" #").unlockedBy("amber", has(ItemRegistry.AMBER.get())).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ItemRegistry.AMBER_PICKAXE.get()).define('#', Items.STICK).define('X', ItemRegistry.AMBER.get()).pattern("XXX").pattern(" # ").pattern(" # ").unlockedBy("amber", has(ItemRegistry.AMBER.get())).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ItemRegistry.AMBER_SHOVEL.get()).define('#', Items.STICK).define('X', ItemRegistry.AMBER.get()).pattern("X").pattern("#").pattern("#").unlockedBy("amber", has(ItemRegistry.AMBER.get())).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.AMBER_SWORD.get()).define('#', Items.STICK).define('X', ItemRegistry.AMBER.get()).pattern("X").pattern("X").pattern("#").unlockedBy("amber", has(ItemRegistry.AMBER.get())).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.MARAUDER_HELM.get())
+                .pattern("###")
+                .pattern("# #")
+                .define('#', ItemRegistry.HOGLIN_HIDE.get())
+                .unlockedBy("hoglin_hide", has(ItemRegistry.HOGLIN_HIDE.get()))
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.MARAUDER_CAPE.get())
+                .pattern("# #")
+                .pattern("###")
+                .pattern("###")
+                .define('#', ItemRegistry.HOGLIN_HIDE.get())
+                .unlockedBy("hoglin_hide", has(ItemRegistry.HOGLIN_HIDE.get()))
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.MARAUDER_PANTS.get())
+                .pattern("###")
+                .pattern("# #")
+                .pattern("# #")
+                .define('#', ItemRegistry.HOGLIN_HIDE.get())
+                .unlockedBy("hoglin_hide", has(ItemRegistry.HOGLIN_HIDE.get()))
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.MARAUDER_BOOTS.get())
+                .pattern("# #")
+                .pattern("# #")
+                .define('#', ItemRegistry.HOGLIN_HIDE.get())
+                .unlockedBy("hoglin_hide", has(ItemRegistry.HOGLIN_HIDE.get()))
+                .save(consumer);
+
     }
 }
