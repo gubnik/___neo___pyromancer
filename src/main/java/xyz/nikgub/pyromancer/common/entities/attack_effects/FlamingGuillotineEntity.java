ab3762192ffd059e13496f52cab3605c6100cac4
nikgub_ <danger8217.nik@gmail.com>
2024-06-26 04:22:36 +0300

    Added addToLevelForPlayerAt method implementation

--- src/main/java/xyz/nikgub/pyromancer/common/entities/attack_effects/FlamingGuillotineEntity.java
+++ src/main/java/xyz/nikgub/pyromancer/common/entities/attack_effects/FlamingGuillotineEntity.java
@@ -4,14 +4,16 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import xyz.nikgub.incandescent.common.util.EntityUtils;
 import xyz.nikgub.pyromancer.registries.AttributeRegistry;
 import xyz.nikgub.pyromancer.registries.DamageSourceRegistry;
+
+import java.util.Optional;
 
 public class FlamingGuillotineEntity extends AttackEffectEntity {
     public AnimationState fallAnimationState = new AnimationState();
@@ -24,19 +26,25 @@ public class FlamingGuillotineEntity extends AttackEffectEntity {
     @Override
     public void tick()
     {
-        if(this.tickCount > lifetime){
-            this.remove(RemovalReason.DISCARDED);
-            if(this.level() instanceof ServerLevel serverLevel)
-            {
-                serverLevel.sendParticles(ParticleTypes.FLAME, this.getX(), this.getY(), this.getZ(), 30, 0.5, 2, 0.5, 0.3);
-                Entity owner = (this.getPlayerUuid() != null) ? this.level().getPlayerByUUID(this.getPlayerUuid()) : this;
-                if (owner == null) return;
-                for(LivingEntity entity : EntityUtils.entityCollector(this.position(), 3 * Mth.sqrt(this.getSize()), this.level()))
-                {
-                    entity.hurt(DamageSourceRegistry.blazingJournal(this, owner),
-                            ((owner instanceof Player player) ? (float) player.getAttributeValue(AttributeRegistry.PYROMANCY_DAMAGE.get()) : 4f) * this.getSize());
-                }
-            }
+        if (this.tickCount <= lifetime) return;
+        this.remove(RemovalReason.DISCARDED);
+        if (!(this.level() instanceof ServerLevel serverLevel)) return;
+        serverLevel.sendParticles(ParticleTypes.FLAME, this.getX(), this.getY(), this.getZ(), 30, 0.5, 2, 0.5, 0.3);
+        Optional<Player> ownerOpt = this.getOwner();
+        if (ownerOpt.isEmpty()) return;
+        Player owner = ownerOpt.get();
+        for(LivingEntity entity : EntityUtils.entityCollector(this.position(), 3 * Mth.sqrt(this.getSize()), this.level()))
+        {
+            if (entity == owner) continue;
+            entity.hurt(DamageSourceRegistry.blazingJournal(this, owner), ((float) owner.getAttributeValue(AttributeRegistry.PYROMANCY_DAMAGE.get())) * this.getSize());
         }
+    }
+
+    @Override
+    public void addToLevelForPlayerAt(Level level, Player player, Vec3 pos) {
+        this.setPlayerUuid(player.getUUID());
+        this.moveTo(pos);
+        this.setYRot(player.getYRot());
+        player.level().addFreshEntity(this);
     }
 }
