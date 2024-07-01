7a990ec11a9f0521561c33e75ea26b9cad82505c
nikgub_ <danger8217.nik@gmail.com>
2024-07-01 12:40:03 +0300

    Removed redundant variables, added tags for mineables

--- src/main/java/xyz/nikgub/pyromancer/data/BlockTagDatagen.java
+++ src/main/java/xyz/nikgub/pyromancer/data/BlockTagDatagen.java
@@ -18,18 +18,25 @@ import org.jetbrains.annotations.Nullable;
 import java.util.concurrent.CompletableFuture;
 
 public class BlockTagDatagen extends IntrinsicHolderTagsProvider<Block> {
+
     public static final TagKey<Block> FLAMING_GROVE_PLANT_ON = BlockTags.create(new ResourceLocation(PyromancerMod.MOD_ID, "flaming_grove_plant_on"));
     public static final TagKey<Block> FLAMING_GROVE_NATIVE = BlockTags.create(new ResourceLocation(PyromancerMod.MOD_ID, "flaming_grove_native"));
-    public static final TagKey<Block> VANILLA_FENCES = BlockTags.create(new ResourceLocation("minecraft", "fences"));
-    public static final TagKey<Block> VANILLA_FENCE_GATES = BlockTags.create(new ResourceLocation("minecraft", "fence_gates"));
-    public static final TagKey<Block> VANILLA_WOODEN_DOORS = BlockTags.create(new ResourceLocation("minecraft", "wooden_doors"));
-    public static final TagKey<Block> VANILLA_LOGS = BlockTags.create(new ResourceLocation("minecraft", "logs"));
+    public static final TagKey<Block> AMBER_REPLACEABLE = BlockTags.create(new ResourceLocation(PyromancerMod.MOD_ID, "amber_replaceable"));
+
+
     protected BlockTagDatagen(PackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(output, Registries.BLOCK, completableFuture, (p_255627_) -> p_255627_.builtInRegistryHolder().key(), modId, existingFileHelper);
     }
 
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
+
+        this.tag(AMBER_REPLACEABLE)
+                .add(Blocks.NETHERRACK);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(BlockRegistry.AMBER_BLOCK.get())
+                .add(BlockRegistry.NATURAL_AMBER.get());
 
         this.tag(BlockTags.NYLIUM)
                 .add(BlockRegistry.PYROMOSSED_NETHERRACK.get());
@@ -40,8 +47,12 @@ public class BlockTagDatagen extends IntrinsicHolderTagsProvider<Block> {
                 .add(BlockRegistry.PYROMOSSED_NETHERRACK.get());
 
         for(Block block : BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(block -> block.getDescriptionId().contains("pyrowood")).toList())
+        {
+            this.tag(BlockTags.MINEABLE_WITH_AXE)
+                    .add(block);
             this.tag(FLAMING_GROVE_NATIVE)
                     .add(block);
+        }
 
         this.tag(FLAMING_GROVE_NATIVE)
                 .add(BlockRegistry.SIZZLING_VINE.get())
@@ -50,12 +61,12 @@ public class BlockTagDatagen extends IntrinsicHolderTagsProvider<Block> {
 
         /* VANILLA */
         for(Block block : BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(block -> block instanceof FenceBlock).toList())
-            this.tag(VANILLA_FENCES).add(block);
+            this.tag(BlockTags.FENCES).add(block);
         for(Block block : BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(block -> block instanceof FenceGateBlock).toList())
-            this.tag(VANILLA_FENCE_GATES).add(block);
+            this.tag(BlockTags.FENCE_GATES).add(block);
         for(Block block : BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(block -> block.getDescriptionId().contains("log")).toList())
-            this.tag(VANILLA_LOGS).add(block);
+            this.tag(BlockTags.LOGS).add(block);
         for(Block block : BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(block -> block instanceof DoorBlock && DoorBlock.isWoodenDoor(block.defaultBlockState())).toList())
-            this.tag(VANILLA_WOODEN_DOORS).add(block);
+            this.tag(BlockTags.WOODEN_DOORS).add(block);
     }
 }
