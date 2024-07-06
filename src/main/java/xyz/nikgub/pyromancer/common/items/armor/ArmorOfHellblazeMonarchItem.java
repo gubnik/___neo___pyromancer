93be29c13cf5b543a39f58cdcc6e9533fe3c7e95
nikgub_ <danger8217.nik@gmail.com>
2024-07-06 03:16:05 +0300

    Normalized armor attribute behaviour

--- src/main/java/xyz/nikgub/pyromancer/common/items/armor/ArmorOfHellblazeMonarchItem.java
+++ src/main/java/xyz/nikgub/pyromancer/common/items/armor/ArmorOfHellblazeMonarchItem.java
@@ -2,12 +2,14 @@ package xyz.nikgub.pyromancer.common.items.armor;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.Util;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
@@ -17,28 +19,34 @@ import xyz.nikgub.pyromancer.client.models.armor.ArmorOfHellblazeMonarchModel;
 import xyz.nikgub.pyromancer.registries.ArmorMaterialsRegistry;
 import xyz.nikgub.pyromancer.registries.AttributeRegistry;
 
-import java.util.Map;
+import java.util.EnumMap;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 public class ArmorOfHellblazeMonarchItem extends ArmorItem {
 
-    public final Map<EquipmentSlot, UUID> PYROMANCY_DAMAGE_UUID_MAP = Map.of(
-            EquipmentSlot.HEAD, UUID.fromString("6aada1d8-4b50-11ee-be56-0242ac120002"),
-            EquipmentSlot.CHEST, UUID.fromString("6aada1d8-4b50-11ee-be56-0242ac120003"),
-            EquipmentSlot.LEGS, UUID.fromString("6aada1d8-4b50-11ee-be56-0242ac120004"),
-            EquipmentSlot.FEET, UUID.fromString("6aada1d8-4b50-11ee-be56-0242ac120005")
-    );
+    private static final EnumMap<Type, UUID> ARMOR_MODIFIER_UUID_PER_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"));
+        map.put(ArmorItem.Type.LEGGINGS, UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"));
+        map.put(ArmorItem.Type.CHESTPLATE, UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"));
+        map.put(ArmorItem.Type.HELMET, UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150"));
+    });
 
     private final Multimap<Attribute, AttributeModifier> attributes;
 
     public ArmorOfHellblazeMonarchItem(Type type) {
-        super(ArmorMaterialsRegistry.PYROMANCER_ARMOR, type, new Properties().stacksTo(1).fireResistant().rarity(Rarity.UNCOMMON));
+        super(ArmorMaterialsRegistry.HELLBLAZE_MONARCH, type, new Properties().stacksTo(1).fireResistant().rarity(Rarity.UNCOMMON));
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-        EquipmentSlot slot = type.getSlot();
-        builder.putAll(super.getDefaultAttributeModifiers(slot));
+        UUID uuid = ARMOR_MODIFIER_UUID_PER_TYPE.get(type);
+        builder.put(Attributes.ARMOR,
+                new AttributeModifier(uuid, "Armor modifier", this.getDefense(), AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ARMOR_TOUGHNESS,
+                new AttributeModifier(uuid, "Armor toughness", this.getToughness(), AttributeModifier.Operation.ADDITION));
+        if (this.knockbackResistance > 0)
+            builder.put(Attributes.KNOCKBACK_RESISTANCE,
+                    new AttributeModifier(uuid, "Armor knockback resistance", ArmorMaterialsRegistry.HELLBLAZE_MONARCH.getKnockbackResistance(), AttributeModifier.Operation.ADDITION));
         builder.put(AttributeRegistry.PYROMANCY_DAMAGE.get(),
-                new AttributeModifier(PYROMANCY_DAMAGE_UUID_MAP.get(slot), "Pyromancy damage modifier", this.getAttributeValue(slot, AttributeRegistry.PYROMANCY_DAMAGE.get()), AttributeModifier.Operation.MULTIPLY_BASE));
+                new AttributeModifier(uuid, "Pyromancy damage modifier", this.getAttributeValue(type.getSlot(), AttributeRegistry.PYROMANCY_DAMAGE.get()), AttributeModifier.Operation.MULTIPLY_BASE));
         this.attributes = builder.build();
     }
 
