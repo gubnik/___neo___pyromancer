0616a585de14bff41df2a0a43369c5813f24d19e
nikgub_ <danger8217.nik@gmail.com>
2024-08-25 16:01:35 +0300

    Added a wider cave carver

--- src/main/java/xyz/nikgub/pyromancer/data/ConfiguredCarverDatagen.java
+++ src/main/java/xyz/nikgub/pyromancer/data/ConfiguredCarverDatagen.java
@@ -7,8 +7,11 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantFloat;
+import net.minecraft.util.valueproviders.UniformFloat;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.carver.CarverDebugSettings;
 import net.minecraft.world.level.levelgen.carver.CaveCarverConfiguration;
 import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
 import net.minecraft.world.level.levelgen.carver.WorldCarver;
@@ -18,6 +21,7 @@ import xyz.nikgub.pyromancer.PyromancerMod;
 public class ConfiguredCarverDatagen
 {
     public static final ResourceKey<ConfiguredWorldCarver<?>> FLAMING_GROVE_CARVER = createKey("flaming_grove");
+    public static final ResourceKey<ConfiguredWorldCarver<?>> WIDER_CAVE = createKey("wider_cave");
 
     private static ResourceKey<ConfiguredWorldCarver<?>> createKey (String pName)
     {
@@ -33,5 +37,17 @@ public class ConfiguredCarverDatagen
                                 VerticalAnchor.belowTop(1)), ConstantFloat.of(0.6F),
                         VerticalAnchor.aboveBottom(10), holdergetter.getOrThrow(BlockTags.NETHER_CARVER_REPLACEABLES),
                         ConstantFloat.of(1.5F), ConstantFloat.of(1.2F), ConstantFloat.of(-0.7F))));
+        pContext.register(WIDER_CAVE, WorldCarver.CAVE.configured(
+                new CaveCarverConfiguration(0.15F,
+                        UniformHeight.of(VerticalAnchor.aboveBottom(8),
+                                VerticalAnchor.absolute(180)),
+                        UniformFloat.of(0.2F, 1.1F),
+                        VerticalAnchor.aboveBottom(8),
+                        CarverDebugSettings.of(false, Blocks.CRIMSON_BUTTON.defaultBlockState()),
+                        holdergetter.getOrThrow(BlockTags.OVERWORLD_CARVER_REPLACEABLES),
+                        UniformFloat.of(1.1F, 1.6F),
+                        UniformFloat.of(0.9F, 1.7F),
+                        UniformFloat.of(-1.0F, -0.4F))
+        ));
     }
 }
