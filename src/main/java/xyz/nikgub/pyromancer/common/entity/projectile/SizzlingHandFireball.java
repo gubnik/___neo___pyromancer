6eacc799513482970e7b014d54ff3218dbe72379
nikgub_ <danger8217.nik@gmail.com>
2024-08-26 22:59:52 +0300

    Removed I-frame bypass

--- src/main/java/xyz/nikgub/pyromancer/common/entity/projectile/SizzlingHandFireball.java
+++ src/main/java/xyz/nikgub/pyromancer/common/entity/projectile/SizzlingHandFireball.java
@@ -63,7 +63,6 @@ public class SizzlingHandFireball extends Fireball implements ItemSupplier
             {
                 this.doEnchantDamageEffects((LivingEntity) owner, entity);
             }
-            entity.invulnerableTime = 0;
         }
     }
 
