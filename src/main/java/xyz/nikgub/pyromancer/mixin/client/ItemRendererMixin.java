34882d5c17b01a433402ff189c210301c2ba2c9a
nikgub_ <danger8217.nik@gmail.com>
2024-09-29 21:30:50 +0300

    Changed naming convention; it was utterly unreadable

--- src/main/java/xyz/nikgub/pyromancer/mixin/client/ItemRendererMixin.java
+++ src/main/java/xyz/nikgub/pyromancer/mixin/client/ItemRendererMixin.java
@@ -31,11 +31,10 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 @Mixin(ItemRenderer.class)
-@SuppressWarnings("unused")
 public abstract class ItemRendererMixin
 {
     @Unique
-    private static BakedModel pyromancerNew$handleCameraTransforms (PoseStack poseStack, BakedModel model, ItemDisplayContext cameraTransformType, boolean applyLeftHandTransform)
+    private static BakedModel handleCameraTransforms (PoseStack poseStack, BakedModel model, ItemDisplayContext cameraTransformType, boolean applyLeftHandTransform)
     {
         model = model.applyTransform(cameraTransformType, poseStack, applyLeftHandTransform);
         return model;
@@ -47,31 +46,25 @@ public abstract class ItemRendererMixin
     @Shadow
     public abstract BakedModel getModel (ItemStack p_174265_, @Nullable Level p_174266_, @Nullable LivingEntity p_174267_, int p_174268_);
 
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    public void renderMixinHead (ItemStack itemStack, ItemDisplayContext displayContext, boolean b, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo callbackInfo)
-    {
-
-    }
-
     @Inject(method = "render", at = @At("TAIL"), cancellable = true)
     public void renderMixinTail (ItemStack itemStack, ItemDisplayContext displayContext, boolean b, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo callbackInfo)
     {
         if (!(itemStack.getItem() instanceof BlazingJournalItem blazingJournalItem)) return;
-        pyromancerNew$quillRenderManager(itemStack, poseStack, displayContext, multiBufferSource, b, i, j);
+        quillRenderManager(itemStack, poseStack, displayContext, multiBufferSource, b, i, j);
         if (blazingJournalItem instanceof CompendiumOfFlameItem && !itemStack.getOrCreateTag().getBoolean(CompendiumOfFlameItem.IS_OFFHAND))
-            pyromancerNew$pyromancyRenderManager(itemStack, poseStack, displayContext, multiBufferSource, b, i, j);
+            pyromancyRenderManager(itemStack, poseStack, displayContext, multiBufferSource, b, i, j);
         callbackInfo.cancel();
     }
 
     @Unique
-    public void pyromancerNew$quillRenderManager (ItemStack itemStack, PoseStack poseStack, ItemDisplayContext displayContext, MultiBufferSource multiBufferSource, boolean b, int i, int j)
+    public void quillRenderManager (ItemStack itemStack, PoseStack poseStack, ItemDisplayContext displayContext, MultiBufferSource multiBufferSource, boolean b, int i, int j)
     {
         if (!(itemStack.getItem() instanceof BlazingJournalItem blazingJournalItem)) return;
         VertexConsumer vertex;
         ItemStack quill = blazingJournalItem.getItemFromItem(itemStack, 0);
         if (!(quill.getItem() instanceof QuillItem)) quill = new ItemStack(ItemRegistry.BLAZING_QUILL.get());
         BakedModel bakedModelQuill = this.getModel(quill, null, null, (b ? ItemDisplayContext.FIRST_PERSON_LEFT_HAND : ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).ordinal());
-        bakedModelQuill = pyromancerNew$handleCameraTransforms(poseStack, bakedModelQuill, displayContext, b);
+        bakedModelQuill = handleCameraTransforms(poseStack, bakedModelQuill, displayContext, b);
         if (itemStack.getItem() instanceof CompendiumOfFlameItem)
         {
             switch (displayContext)
@@ -107,7 +100,7 @@ public abstract class ItemRendererMixin
     }
 
     @Unique
-    public void pyromancerNew$pyromancyRenderManager (ItemStack itemStack, PoseStack poseStack, ItemDisplayContext displayContext, MultiBufferSource multiBufferSource, boolean b, int i, int j)
+    public void pyromancyRenderManager (ItemStack itemStack, PoseStack poseStack, ItemDisplayContext displayContext, MultiBufferSource multiBufferSource, boolean b, int i, int j)
     {
         if (!(itemStack.getItem() instanceof CompendiumOfFlameItem compendiumOfFlameItem
                 && compendiumOfFlameItem.getItemFromItem(itemStack, itemStack.getOrCreateTag().getInt(CompendiumOfFlameItem.ACTIVE_SLOT_TAG)).getItem() instanceof UsablePyromancyItem usablePyromancyItem)
@@ -117,7 +110,7 @@ public abstract class ItemRendererMixin
         ItemStack pyromancy = compendiumOfFlameItem.getItemFromItem(itemStack, itemStack.getOrCreateTag().getInt(CompendiumOfFlameItem.ACTIVE_SLOT_TAG));
         pyromancy.getOrCreateTag().putBoolean(CompendiumOfFlameItem.PYROMANCY_CUSTOM_RENDER_TAG, true);
         BakedModel bakedModelPyromancy = this.getModel(pyromancy, null, null, (b ? ItemDisplayContext.FIRST_PERSON_LEFT_HAND : ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).ordinal());
-        bakedModelPyromancy = pyromancerNew$handleCameraTransforms(poseStack, bakedModelPyromancy, displayContext, b);
+        bakedModelPyromancy = handleCameraTransforms(poseStack, bakedModelPyromancy, displayContext, b);
         usablePyromancyItem.compendiumTransforms(poseStack, displayContext);
         for (RenderType renderType : bakedModelPyromancy.getRenderTypes(pyromancy, true))
         {
