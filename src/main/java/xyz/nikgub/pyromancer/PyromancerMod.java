7c34464253660fb197108aeefb78aa918c23f69c
nikgub_ <danger8217.nik@gmail.com>
2024-11-24 01:21:02 +0300

    Added GNU GPL v3 license notifications

--- src/main/java/xyz/nikgub/pyromancer/PyromancerMod.java
+++ src/main/java/xyz/nikgub/pyromancer/PyromancerMod.java
@@ -1,6 +1,6 @@
 /*
     Pyromancer, Minecraft Forge modification
-    Copyright (C) 2024, nikgub_
+    Copyright (C) 2024, Nikolay Gubankov (aka nikgub)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package xyz.nikgub.pyromancer;
 
 import com.mojang.logging.LogUtils;
--- src/main/java/xyz/nikgub/pyromancer/client/renderer/entity/PyracornRender.java
+++ src/main/java/xyz/nikgub/pyromancer/client/renderer/entity/PyracornRender.java
@@ -1,3 +1,20 @@
+/*
+    Pyromancer, Minecraft Forge modification
+    Copyright (C) 2024, Nikolay Gubankov (aka nikgub)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package xyz.nikgub.pyromancer.client.renderer.entity;
 
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
