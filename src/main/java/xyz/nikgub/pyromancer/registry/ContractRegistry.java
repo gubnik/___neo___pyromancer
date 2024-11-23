d604731b115a761f78c7e21059e4e5a6f09c46da
nikgub_ <danger8217.nik@gmail.com>
2024-11-24 00:45:36 +0300

    Removed useless methods

--- src/main/java/xyz/nikgub/pyromancer/registry/ContractRegistry.java
+++ src/main/java/xyz/nikgub/pyromancer/registry/ContractRegistry.java
@@ -11,29 +11,13 @@ import net.minecraftforge.registries.RegistryObject;
 import xyz.nikgub.pyromancer.PyromancerMod;
 import xyz.nikgub.pyromancer.common.contract.AccursedContractEntry;
 
-import java.util.Set;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class ContractRegistry
 {
     public static final ResourceKey<Registry<AccursedContractEntry<?>>> REGISTRY_KEY = ResourceKey.createRegistryKey(new ResourceLocation(PyromancerMod.MOD_ID, "contracts"));
     public static final DeferredRegister<AccursedContractEntry<?>> CONTRACTS = DeferredRegister.create(REGISTRY_KEY, PyromancerMod.MOD_ID);
     public static final Supplier<IForgeRegistry<AccursedContractEntry<?>>> REGISTRY = CONTRACTS.makeRegistry(() -> new RegistryBuilder<AccursedContractEntry<?>>().disableOverrides());
-
-    public static RegistryObject<AccursedContractEntry<?>> registerContractEntry (String name, AccursedContractEntry<?> entry)
-    {
-        return CONTRACTS.register(name, () -> entry);
-    }
-
-    public static Set<AccursedContractEntry<?>> getFromEntityType (EntityType<?> entityType)
-    {
-        return REGISTRY.get()
-                .getValues()
-                .stream()
-                .filter(accursedContractEntry -> accursedContractEntry.getEntityToSummon() == entityType)
-                .collect(Collectors.toSet());
-    }
 
     public static RegistryObject<AccursedContractEntry<?>> ZOMBIE = CONTRACTS.register("zombie",
             () -> new AccursedContractEntry<>(EntityType.ZOMBIE, 1));
