c20d78726a2616707eaa42cd218b02f32507328c
nikgub_ <danger8217.nik@gmail.com>
2024-12-31 01:07:04 +0300

    Fix left-hand animation

--- src/main/java/xyz/nikgub/pyromancer/client/item_extension/MusketClientExtension.java
+++ src/main/java/xyz/nikgub/pyromancer/client/item_extension/MusketClientExtension.java
@@ -92,7 +92,7 @@ public class MusketClientExtension implements IClientItemExtensions
     private void applyLoadingHandTransforms (PoseStack poseStack, HumanoidArm arm, float v)
     {
         int i = arm == HumanoidArm.RIGHT ? 1 : -1;
-        poseStack.rotateAround(Axis.ZP.rotationDegrees(120 + (i == 1 ? 0 : 180)), (float) i * 0.56F, -0.52F + v * -0.6F, -0.72F);
+        poseStack.rotateAround(Axis.ZP.rotationDegrees(120 + (i == 1 ? 0 : 90)), (float) i * 0.56F, -0.52F + v * -0.6F, -0.72F);
         poseStack.rotateAround(Axis.YP.rotationDegrees(90 * i), (float) i * 0.56F, -0.52F + v * -0.6F, -0.72F);
         poseStack.translate((float) i * 0.56F, -0.52F + v * -0.6F, -0.72F);
     }
