772915dcc7665039a92c1f8166003fd44a478258
nikgub_ <danger8217.nik@gmail.com>
2025-07-14 01:21:57 +0300

    Add description to Zweihander

--- src/main/java/xyz/nikgub/pyromancer/common/item/ZweihanderItem.java
+++ src/main/java/xyz/nikgub/pyromancer/common/item/ZweihanderItem.java
@@ -25,6 +25,7 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
@@ -38,6 +39,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ForgeMod;
@@ -46,13 +48,16 @@ import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.NotNull;
 import xyz.nikgub.incandescent.common.item_interfaces.IBetterAttributeTooltipItem;
 import xyz.nikgub.incandescent.common.item_interfaces.ICustomSwingItem;
+import xyz.nikgub.incandescent.common.item_interfaces.IExtensibleTooltipItem;
 import xyz.nikgub.incandescent.util.Hypermap;
+import xyz.nikgub.pyromancer.PyromancerConfig;
 import xyz.nikgub.pyromancer.registry.EnchantmentRegistry;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-public class ZweihanderItem extends Item implements IBetterAttributeTooltipItem, ICustomSwingItem
+public class ZweihanderItem extends Item implements IBetterAttributeTooltipItem, ICustomSwingItem, IExtensibleTooltipItem
 {
 
     public static final Set<ToolAction> ACTIONS = Set.of(ToolActions.SWORD_SWEEP);
@@ -176,6 +181,13 @@ public class ZweihanderItem extends Item implements IBetterAttributeTooltipItem,
             Attributes.ATTACK_SPEED, Item.BASE_ATTACK_SPEED_UUID, this.defaultStyle(itemStack),
             ForgeMod.ENTITY_REACH.get(), REACH_UUID, this.defaultStyle(itemStack)
         );
+    }
+
+    @Override
+    public void appendHoverText (@NotNull ItemStack itemStack, @javax.annotation.Nullable Level level, @NotNull List<Component> list, @NotNull TooltipFlag flag)
+    {
+        this.gatherTooltipLines(list, "pyromancer.hidden_desc", "desc", PyromancerConfig.descTooltipKey);
+        this.gatherTooltipLines(list, "pyromancer.hidden_lore", "lore", PyromancerConfig.loreTooltipKey);
     }
 
     @Override
